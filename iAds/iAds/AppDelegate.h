955b821b3a4a6242b72391a4a1535d4738ab31bd
hechoencolombia <Alejo@Alejos-MacBook-Pro.local>
2014-07-26 13:39:41 -0400

    initial

---
+++ iAds/iAds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iAds
+//
+//  Created by Alejo on 2014-07-26.
+//  Copyright (c) 2014 AlejoApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
